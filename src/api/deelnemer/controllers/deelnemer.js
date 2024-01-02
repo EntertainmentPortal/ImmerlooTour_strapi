9aa5b906c5a43df0834bbf7292ad50d20a976bd6
EntertainmentPortal <jeffrey@ikuu.io>
2024-01-02 17:25:35 +0100

    Mid of day commit (UNSTABLE, preps for new uitslagen)

---
+++ src/api/deelnemer/controllers/deelnemer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * deelnemer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::deelnemer.deelnemer');
---
+++ src/api/deelnemer/routes/deelnemer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * deelnemer router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::deelnemer.deelnemer');
